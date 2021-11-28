6cf6357af79be7fcce0b29012f3acc5dbcb2984f
이총명 <dlchdaud1327@gmail.com>
2021-11-28 13:19:51 +0900

    - navigator

--- flutter_study7/lib/main.dart
+++ flutter_study7/lib/main.dart
@@ -1,18 +1,60 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: FirstPage()
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context2) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First page'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Go to the Second page'),
+          onPressed: (){
+            Navigator.push(context2,
+                MaterialPageRoute(
+                  builder: (_) => SecondPage()
+            ));
+          }
+        ),
+      )
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext ctx) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Second page'),
+        ),
+        body: Center(
+          child: RaisedButton(
+              child: Text('Go to the First page'),
+              onPressed: (){
+                Navigator.pop(ctx);
+              }
+          ),
+        )
     );
   }
 }
